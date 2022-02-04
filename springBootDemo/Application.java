5f074346f98a598deeb89875f9090fd5ad04371f
Antonio Salazar Graván <antsalgra@hotmail.com>
2022-02-04 21:28:05 +0100

    Corregido error de sintaxis el aplicación java

--- springBootDemo/Application.java
+++ springBootDemo/Application.java
@@ -11,7 +11,7 @@ import org.springframework.web.bind.annotation.RestController;
 public class DemoApplication {
     @RequestMapping("/")
     public String home() {
-        return "Hello Docker World";
+        return "Hola mundo!";
     }
  
     public static void main(String[] args) {
